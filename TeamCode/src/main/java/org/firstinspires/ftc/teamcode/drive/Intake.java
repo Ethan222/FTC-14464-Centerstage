a6b3a958ef1938664c96ea673fedf6e79d8d249c
Ethan222 <ethannworks@gmail.com>
2023-12-13 17:36:58 -0500

    Update Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Intake.java
@@ -2,24 +2,20 @@ package org.firstinspires.ftc.teamcode.drive;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-// class to control the intake motor
+// class to control the intake mechanism
 public class Intake extends Motor { // inherits from parent class Motor
     public CustomServo lowerer;     // servo to raise/lower the intake
     private static final double UP_PSN = 1, DOWN_PSN = 0;   // up and down positions of the servo
     public Intake(HardwareMap hardwareMap, String name) {        // constructor
-        super(hardwareMap, name, .8, .9);   // calls parent constructor
+        super(hardwareMap, name, .8, .9);   // calls parent constructor to initialize intake motor
         lowerer = new CustomServo(hardwareMap, "servo2", DOWN_PSN, UP_PSN); // initializes lowerer servo
     }
-    // positive intakes
     public void in(double power) {
         setPower(power);
-    }
-    // function overload: if no power given, use the default speed
-    public void in() { in(DEFAULT_SPEED); }
-    // negative outtakes
-    public void out(double power) { setPower(-power); }
-    // function overload: if no power given, use the default speed
-    public void out() { out(DEFAULT_SPEED); }
+    } // positive intakes
+    public void in() { in(DEFAULT_SPEED); }     // function overload: if no power given, use the default speed
+    public void out(double power) { setPower(-power); } // negative outtakes
+    public void out() { out(DEFAULT_SPEED); }   // function overload: if no power given, use the default speed
     public void lower() { // lower intake
         lowerer.setPosition(DOWN_PSN);
     }
