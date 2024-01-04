88381dbe9b60e112e6e6875791d4bbdb93d05f82
Ethan222 <ethannworks@gmail.com>
2024-01-04 16:23:17 -0500

    configured robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.subsystems.Robot;
 
@@ -21,7 +22,8 @@ public class TeleOp extends LinearOpMode {
 
         Button x = new Button(), y = new Button();
 
-        boolean singleDriverMode = false;
+        boolean singleDriverMode = true;
+        Gamepad gamepad;
         while(opModeInInit()) {
             telemetry.addLine("Initialized");
             telemetry.addData("Single driver mode", singleDriverMode);
@@ -53,95 +55,86 @@ public class TeleOp extends LinearOpMode {
             else if((gamepad1.back && gamepad1.b) || (gamepad2.back && gamepad2.b))
                 singleDriverMode = false;
 
+            if(singleDriverMode)
+                gamepad = gamepad1;
+            else
+                gamepad = gamepad2;
+
             // bumpers raise/lower the intake
-            if(gamepad2.right_bumper || (singleDriverMode && gamepad1.right_bumper))
+            if(gamepad.right_bumper)
                 robot.intake.lower();
-            else if(gamepad2.left_bumper || (singleDriverMode && gamepad1.left_bumper))
+            else if(gamepad.left_bumper)
                 robot.intake.raise();
 
             // use the triggers to control the intake - right trigger intakes, left trigger outtakes
-            if(gamepad2.right_trigger > 0)
-                robot.intake.in(gamepad2.right_trigger);
-            else if(gamepad2.left_trigger > 0)
-                robot.intake.out(gamepad2.left_trigger);
-            else if(singleDriverMode && gamepad1.right_trigger > 0)
-                robot.intake.in(gamepad1.right_trigger);
-            else if(singleDriverMode && gamepad1.left_trigger > 0)
-                robot.intake.out(gamepad1.left_trigger);
+            if(gamepad.right_trigger > 0)
+                robot.intake.in(gamepad.right_trigger);
+            else if(gamepad.left_trigger > 0)
+                robot.intake.out(gamepad.left_trigger);
             else
                 robot.intake.stop();
 
-            // right stick y or d-pad up/down controls the arm lifter
-            if(gamepad2.right_stick_y != 0)
-                robot.armRaisers.setPower(-gamepad2.right_stick_y);
-            else if(gamepad2.dpad_up || (singleDriverMode && gamepad1.dpad_up))
+            // left stick y or d-pad up/down controls the arm lifter
+            if(gamepad2.left_stick_y != 0)
+                robot.armRaisers.setPower(-gamepad2.left_stick_y);
+            else if(gamepad.dpad_up)
                 robot.armRaisers.up();
-            else if(gamepad2.dpad_down || (singleDriverMode && gamepad1.dpad_down))
+            else if(gamepad.dpad_down)
                 robot.armRaisers.down();
             else
                 robot.armRaisers.stop();
 
-            // left stick y or d pad left/right controls arm flipper
-            if(gamepad2.left_stick_y != 0)
-                robot.armFlipper.flip(-gamepad2.left_stick_y);
-            else if(gamepad2.dpad_right || (singleDriverMode && gamepad1.dpad_right))
-                robot.armFlipper.flip();
-            else if(gamepad2.dpad_left || (singleDriverMode && gamepad1.dpad_left))
-                robot.armFlipper.unflip();
-            else
-                robot.armFlipper.stop();
-
             // a/b grip/ungrip fully
-            if(gamepad2.a && !gamepad2.start && !gamepad2.back)
-                robot.gripper.gripFully();
-            else if(gamepad2.b && !gamepad2.start && !gamepad2.back)
-                robot.gripper.ungripFully();
-            else if(singleDriverMode && gamepad1.a && !gamepad1.start && !gamepad1.back)
-                robot.gripper.gripFully();
-            else if(singleDriverMode && gamepad1.b && !gamepad1.start && !gamepad1.back)
-                robot.gripper.ungripFully();
+//            if(gamepad2.a && !gamepad2.start && !gamepad2.back)
+//                robot.gripper.gripFully();
+//            else if(gamepad2.b && !gamepad2.start && !gamepad2.back)
+//                robot.gripper.ungripFully();
+//            else if(singleDriverMode && gamepad1.a && !gamepad1.start && !gamepad1.back)
+//                robot.gripper.gripFully();
+//            else if(singleDriverMode && gamepad1.b && !gamepad1.start && !gamepad1.back)
+//                robot.gripper.ungripFully();
 
             // x/y rotate
             // short press moves at half speed, after half a second it starts moving at full speed
-            if(gamepad2.y || (singleDriverMode && gamepad1.y)) {
-                y.down();
-                if(y.getTimeDown() < 500) // short press
-                    robot.rotator.rotate(.5); // just move a little bit (small adjustments)
-                else // long press
-                    robot.rotator.rotate(1); // move as fast as possible
-            } else if(gamepad2.x || (singleDriverMode && gamepad1.x)) {
-                x.down();
-                if(x.getTimeDown() < 500)
-                    robot.rotator.unrotate(.5);
-                else
-                    robot.rotator.unrotate(1);
-            } else {
-                x.up();
-                y.up();
-                robot.rotator.stop();
-            }
+//            if(gamepad2.y || (singleDriverMode && gamepad1.y)) {
+//                y.down();
+//                if(y.getTimeDown() < 500) // short press
+//                    robot.rotator.rotate(.5); // just move a little bit (small adjustments)
+//                else // long press
+//                    robot.rotator.rotate(1); // move as fast as possible
+//            } else if(gamepad2.x || (singleDriverMode && gamepad1.x)) {
+//                x.down();
+//                if(x.getTimeDown() < 500)
+//                    robot.rotator.unrotate(.5);
+//                else
+//                    robot.rotator.unrotate(1);
+//            } else {
+//                x.up();
+//                y.up();
+//                robot.rotator.stop();
+//            }
 
             // gamepad1 a/b control launcher
-            if(gamepad1.a && !singleDriverMode && !gamepad1.start && !gamepad1.back)
-                robot.launcher.launch();
-            else if(gamepad1.b && !singleDriverMode && !gamepad1.start && !gamepad1.back)
-                robot.launcher.reset();
+//            if(gamepad1.a && !singleDriverMode && !gamepad1.start && !gamepad1.back)
+//                robot.launcher.launch();
+//            else if(gamepad1.b && !singleDriverMode && !gamepad1.start && !gamepad1.back)
+//                robot.launcher.reset();
 
             telemetry.addData("Single driver mode", singleDriverMode);
             telemetry.addLine("\nintake: triggers (raise/lower intake: bumpers)");
-            telemetry.addLine("arm: joysticks or d-pad");
-            telemetry.addLine("gripper: a/b");
-            telemetry.addLine("rotator: x/y");
-            telemetry.addLine("launcher: gamepad1 a/b");
+            telemetry.addLine("arm: left joystick or d-pad up/down");
+//            telemetry.addLine("gripper: a/b");
+//            telemetry.addLine("rotator: x/y");
+//            telemetry.addLine("launcher: gamepad1 a/b");
             telemetry.addData("\nGamepad1 left_stick_y", gamepad1.left_stick_y);
             telemetry.addData("Average wheel power", robot.drive.getAverageDriveMotorPower());
             telemetry.addData("\nIntake power", robot.intake.getPower());
-            telemetry.addData("Arm flipper power", robot.armFlipper.getPower());
-            telemetry.addData("Arm lifter powers", robot.armRaisers.getPower());
-            telemetry.addData("Rotator power", "%.2f", robot.rotator.getPower());
-            telemetry.addData("Gripper psn", "%s (%.3f)", robot.gripper.getStatus(), robot.gripper.getPosition());
+//            telemetry.addData("Arm flipper power", robot.armFlipper.getPower());
+            telemetry.addData("Arm raiser power", robot.armRaisers.getPower());
+//            telemetry.addData("Rotator power", "%.2f", robot.rotator.getPower());
+//            telemetry.addData("Gripper psn", "%s (%.3f)", robot.gripper.getStatus(), robot.gripper.getPosition());
             telemetry.addData("Intake psn", "%s (%.1f)", robot.intake.getStatus(), robot.intake.lowerer.getPosition());
-            telemetry.addData("Launcher psn", "%.2f", robot.launcher.getPosition());
+//            telemetry.addData("Launcher psn", "%.2f", robot.launcher.getPosition());
             telemetry.update();
         }
     }
